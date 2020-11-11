e3c609b9e284cf868fdabbefd6d585787be5556e
David Moina <davemolina777@gmail.com>
2020-11-11 15:13:14 -0600

    commit tree 12bfca4da0b64507cdaeeee610f8ca7def80c5b1

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
 # your code goes here
+class Person
+    attr_accessor :bathe, :call, :speak, :hygiene, :bank_account
+    attr_reader :name , :happiness
+    attr_writer :bank_account
+    
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    def happiness=(happiness)
+        @happiness=happiness
+        happiness == [0..10]    
+    end        
+    # def get_paid
+    # bank_account += 1
+    # "all about the benjamins"
+    # end
+    # def self.happiness
+    #     if happiness == [0..10]
+    #         true
+    #     else 
+    #         false
+    #     end
+   # end
+
+
+end
